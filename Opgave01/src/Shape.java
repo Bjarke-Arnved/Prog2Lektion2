3b75382b4159daf48e27bee07c33b9786b250f85
Bjarke <eaa23bjar@students.eaaa.dk>
2024-02-05 11:44:47 +0100

    Added default constructor to Shape

--- Opgave01/src/Shape.java
+++ Opgave01/src/Shape.java
@@ -2,7 +2,11 @@ public class Shape {
     private int xAxis;
     private int yAxis;
     private int area;
-
+    public Shape() {
+        xAxis = 0;
+        yAxis = 0;
+        area = 0;
+    }
     public Shape(int xAxis, int yAxis, int area) {
         this.xAxis = xAxis;
         this.yAxis = yAxis;
